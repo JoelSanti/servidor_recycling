ecc8931400cde0724c1f8e30e02c1159af7f0eea
joel <joeljhojan9@gmail.com>
2024-07-27 18:37:25 -0500

    feat: create user resident function build

--- dist/collections/Users.js
+++ dist/collections/Users.js
@@ -3,6 +3,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var Users = {
     slug: 'users',
     auth: true,
+    access: {
+        create: function (_a) {
+            var req = _a.req;
+            // Permitir la creación de usuarios si el usuario no está autenticado
+            // o si el usuario autenticado tiene el rol 'admin'
+            return !req.user || req.user.role === 'admin';
+        },
+        // Configurar el acceso para otras operaciones (read, update, delete) según sea necesario
+    },
     admin: {
         useAsTitle: 'email',
     },
@@ -31,5 +40,18 @@ var Users = {
         },
         // Add more fields as needed
     ],
+    hooks: {
+        beforeChange: [
+            function (_a) {
+                var req = _a.req, data = _a.data;
+                // Si el usuario no está autenticado y está intentando crear un usuario 'admin',
+                // lanzar un error
+                if (!req.user && data.role === 'admin') {
+                    throw new Error('Unauthenticated users cannot create admin users');
+                }
+                return data;
+            },
+        ],
+    },
 };
 exports.default = Users;
